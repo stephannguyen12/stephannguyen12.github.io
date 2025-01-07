6fcf36566e842af2177322a59751dd0be465cd85
stephannguyen12 <sngtx555@gmail.com>
2025-01-07 13:11:45 -0600

    added disclosure

---
+++ src/pages/DisclosureGridData.tsx
@@ -0,0 +1,22 @@
+import page0 from       '../files/disclosure/disclosure.jpg'
+import page1 from       '../files/disclosure/disclosure2.jpg'
+import page2 from       '../files/disclosure/disclosure3.jpg'
+import page3 from       '../files/disclosure/disclosure4.jpg'
+import page4 from       '../files/disclosure/disclosure5.jpg'
+import page5 from       '../files/disclosure/disclosure6.jpg'
+import page6 from       '../files/disclosure/disclosure7.jpg'
+import page7 from       '../files/disclosure/disclosure8.jpg'
+import page8 from       '../files/disclosure/disclosure9.jpg'
+
+
+export const data = [
+    {image: page0,},
+    {image: page1},
+    {image: page2},
+    {image: page3},
+    {image: page4},
+    {image: page5},
+    {image: page6},
+    {image: page7},
+    {image: page8},
+]
